94a5e37c82113ad93f0308ae80fde3e9101c3c19
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-19 19:04:57 +0300

    Added print statemets for debugging

--- application/jobs.py
+++ application/jobs.py
@@ -1,12 +1,22 @@
 import os
 
+print("\n\n{}\n\n".format("This is jobs.py"))
+
 def printer(text="Here"):
     print("\n\n{}\n\n".format(text))
 
 if os.environ.get("HEROKU"):
+    print("\n\n{}\n\n".format("This is a heroku environment"))
     from apscheduler.schedulers.blocking import BlockingScheduler
     scheduler = BlockingScheduler()
 
+    @scheduler.scheduled_job('interval', minutes=2)
+    def timed_job():
+        print('This job is run every three minutes.')
+
     scheduler.start()
     scheduler.add_job(printer, 'interval', minutes=1, kwargs={"text":"This is a heroku job"})
+
+else:
+    print("\n\n{}\n\n".format("Something happened"))
 
