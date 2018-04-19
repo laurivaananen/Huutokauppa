4e50e90deda1b55ae4875d2787980098049f5624
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-19 20:34:35 +0300

    Trying to shutdown jobs

--- application/jobs.py
+++ application/jobs.py
@@ -5,16 +5,16 @@ print("\n\n{}\n\n".format("This is jobs.py"))
 def printer(text="Here"):
     print("\n\n{}\n\n".format(text))
 
-if os.environ.get("HEROKU"):
-    # print("\n\n{}\n\n".format("This is a heroku environment"))
-    # from apscheduler.schedulers.blocking import BlockingScheduler
-    # scheduler = BlockingScheduler()
+# if os.environ.get("HEROKU"):
+from apscheduler.schedulers.blocking import BlockingScheduler
+scheduler = BlockingScheduler()
 
-    # @scheduler.scheduled_job('interval', minutes=2)
-    # def timed_job():
-    #     print('This job is run every three minutes.')
-    # print("\n\n{}\n\n".format("Starting a scheduler"))
-    # scheduler.start()
+@scheduler.scheduled_job('interval', minutes=2)
+def timed_job():
+    print('This job is run every three minutes.')
+    
+print("\n\n{}\n\n".format("Starting a scheduler"))
+scheduler.start()
     # print("\n\n{}\n\n".format("Scheduler started!"))
     # scheduler.shutdown()
     # scheduler.add_job(printer, 'interval', minutes=1, id="heye", kwargs={"text":"This is a heroku job"})
@@ -23,21 +23,18 @@ if os.environ.get("HEROKU"):
     
     # scheduler.print_jobs()
 
-    from apscheduler.schedulers.background import BackgroundScheduler
-    scheduler = BackgroundScheduler()
+# from apscheduler.schedulers.background import BackgroundScheduler
+# scheduler = BackgroundScheduler()
 
-    @scheduler.scheduled_job('interval', minutes=2)
-    def timed_job():
-        print('This job is run every three minutes.')
+# @scheduler.scheduled_job('interval', minutes=2)
+# def timed_job():
+#     print('This job is run every three minutes.')
 
-    print("\n\n{}\n\n".format("Starting a scheduler"))
-    scheduler.start()
-    print("\n\n{}\n\n".format("Scheduler started!"))
+# print("\n\n{}\n\n".format("Starting a scheduler"))
+# scheduler.start()
+# print("\n\n{}\n\n".format("Scheduler started!"))
 
-    scheduler.add_job(printer, 'interval', minutes=1, id="heye", kwargs={"text":"This is a heroku job"})
+# scheduler.add_job(printer, 'interval', minutes=1, id="heye", kwargs={"text":"This is a heroku job"})
 
-    scheduler.print_jobs()
-
-else:
-    print("\n\n{}\n\n".format("Something happened"))
+# scheduler.print_jobs()
 
