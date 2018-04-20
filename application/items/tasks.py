c9af2ff7fb7fea5b8f8f76d8f2827e2cb0a43fc9
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-20 03:09:35 +0300

    db session now closes before worker

--- application/items/tasks.py
+++ application/items/tasks.py
@@ -3,6 +3,7 @@ from application.items.models import Item
 
 @celery.task()
 def sell_item(item_id):
-    item = Item.query.get(item_id)
-    item.sold = True
-    db.session().commit()
+        db.session().close()
+        item = Item.query.get(item_id)
+        item.sold = True
+        db.session().commit()
