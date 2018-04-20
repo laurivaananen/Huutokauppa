5024863d0d83ee51bfb89e130f51f747f69d607c
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-20 03:24:28 +0300

    Now inside try

--- application/items/tasks.py
+++ application/items/tasks.py
@@ -3,8 +3,11 @@ from application.items.models import Item
 
 @celery.task()
 def sell_item(item_id):
+    try:
         db.session().close()
         item = Item.query.get(item_id)
         item.sold = True
         db.session().commit()
         db.session().close()
+    except:
+        pass
