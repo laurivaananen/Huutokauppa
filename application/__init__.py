30db5feee931a60cdec126a99b59f4057afd39af
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-20 19:24:42 +0300

    Added db earlier to init

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,8 @@ application = Flask(__name__)
 # Tietokanta
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy(application)
+
 import os
 if os.environ.get("HEROKU"):
     application.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
@@ -12,11 +14,35 @@ if os.environ.get("HEROKU"):
     application.config["CELERY_RESULT_BACKEND"] = os.environ.get("REDIS_URL")
 else:
     application.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
+<<<<<<< 718b4574c1622dc7686ba99ae557849182e23b00
     application.config["SQLALCHEMY_ECHO"] = True
     application.config["CELERY_BROKER_URL"] = "redis://localhost:6379/0"
     application.config["CELERY_RESULT_BACKEND"] = "redis://localhost:6379/0"
+=======
+
+
+    from application import views
+
+    from application.items import models
+    from application.items import views
+
+    from application.bid import models, views
+
+    from application.auth import models
+    from application.auth import views
+
+
+
+<<<<<<< cd1fe4cd014bdbf142128e0266910dfe0b2fd341
+
+
+
+
+>>>>>>> Added db earlier to init
 
 db = SQLAlchemy(application)
+=======
+>>>>>>> Added db earlier to init
 
 # Celery
 from application.tasks import make_celery
