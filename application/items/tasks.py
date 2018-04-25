6dbd01add2ed53026147d3b23fa1b4d50ae320fe
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-25 03:48:02 +0300

    Debugging sell_item task

--- application/items/tasks.py
+++ application/items/tasks.py
@@ -4,9 +4,10 @@ from application.items.models import Item
 @celery.task
 def sell_item(item_id):
     try:
-        db.session().close()
         item = Item.query.get(item_id)
         item.sold = True
+        item.name = "THIS ITEM HAS BEEN SOLD"
+        print("Hello")
         db.session().commit()
         db.session().close()
     except:
