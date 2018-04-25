03d9b7021a0390ccd57d06a940c7e954af8aff01
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-25 19:21:42 +0300

    Reading redis url from environment varibales

--- application/items/tasks.py
+++ application/items/tasks.py
@@ -3,13 +3,7 @@ from application.items.models import Item
 
 @celery.task
 def sell_item(item_id):
-    try:
-        item = Item.query.get(item_id)
-        item.sold = True
-        item.name = "THIS ITEM HAS BEEN SOLD"
-        print("Hello")
-        db.session().commit()
-        db.session().close()
-    except:
-        pass
-
+    item = Item.query.get(item_id)
+    item.sold = True
+    db.session().commit()
+    db.session().close()
--- celery_config.py
+++ celery_config.py
@@ -1,4 +1,7 @@
 import os
 
-broker_url = "redis://flaskredis.jspvaj.0001.usw2.cache.amazonaws.com:6379/0"
-result_backend = "redis://flaskredis.jspvaj.0001.usw2.cache.amazonaws.com:6379/0"
+broker_url = os.environ.get("REDIS_URL")
+result_backend = os.environ.get("REDIS_URL")
+
+# broker_url = "redis://flaskredis.jspvaj.0001.usw2.cache.amazonaws.com:6379/0"
+# result_backend = "redis://flaskredis.jspvaj.0001.usw2.cache.amazonaws.com:6379/0"
