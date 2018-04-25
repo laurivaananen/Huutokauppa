751476c2d2a3f1c9bbc3e3557ef68cde9465638c
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-25 17:07:06 +0300

    Removed file path of supervisorctl from 99_restart_services

--- .ebextensions/files/99_restart_services.sh
+++ .ebextensions/files/99_restart_services.sh
@@ -3,9 +3,9 @@
 set -xe
 
 # check if we already have the celeryd service
-/usr/bin/supervisorctl -c /opt/python/etc/supervisord.conf status | grep celeryd
+supervisorctl -c /opt/python/etc/supervisord.conf status | grep celeryd
 if [[ $? ]]; then
-  /usr/bin/supervisorctl -c /opt/python/etc/supervisord.conf restart celeryd
+  supervisorctl -c /opt/python/etc/supervisord.conf restart celeryd
 fi
 
 eventHelper.py --msg "Application server successfully restarted." --severity INFO
