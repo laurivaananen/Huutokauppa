1705bc4fd5727141b7a15ac6a7d71c73c03a29f0
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-27 00:23:26 +0300

    Updated css to more responsive grid

--- application/items/views.py
+++ application/items/views.py
@@ -25,7 +25,7 @@ def items_form():
 def item_detail(item_id):
 
     item = Item.query.get(item_id)
-    bids = Bid.query.filter_by(item_id=item_id)
+    bids = Bid.query.filter_by(item_id=item_id).order_by(Bid.amount.desc())
 
     return render_template("items/detail.html", item=item, form=BidForm(), bids=bids)
 
