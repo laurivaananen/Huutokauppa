4ab0ca5dc9ab09862f709d9dd4edc6f4731a2ff7
Lauri Väänänen <lauri.e.helsinki.fi>
2018-04-27 15:57:48 +0300

    Password must be 8 characters long. Checks if email already exists in database

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -23,13 +23,19 @@ class UserSignupForm(FlaskForm):
     last_name = StringField("Last name", [validators.InputRequired(), validators.Length(max=144)])
     user_name = StringField("User name", [validators.InputRequired(), validators.Length(max=144)])
     email_address = StringField("Email address", [validators.InputRequired(), validators.Email(), validators.Length(max=144)])
-    password = PasswordField("Password", [validators.InputRequired(), validators.Length(max=144), validators.EqualTo("repeat_password", message="Passwords must match")])
+    password = PasswordField("Password", [validators.InputRequired(), validators.Length(min=8, max=144), validators.EqualTo("repeat_password", message="Passwords must match")])
     repeat_password = PasswordField("Repeat password", [validators.InputRequired()])
     phone_number = StringField("Phone number", [validators.InputRequired(), validators.Length(max=144)])
     country = StringField("Country", [validators.InputRequired(), validators.Length(max=144)])
     city = StringField("City", [validators.InputRequired(), validators.Length(max=144)])
     postal_code = StringField("Postal code", [validators.InputRequired(), validators.Length(max=144)])
     street_address = StringField("Street address", [validators.InputRequired(), validators.Length(max=144)])
+
+    def validate_email_address(form, field):
+        if form.email_address.validate(form):
+            user = AccountInformation.query.filter_by(email_address=form.email_address.data).first()
+            if user:
+                raise ValidationError("User already exists with that email address")
 
     class Meta:
         csrf = False
